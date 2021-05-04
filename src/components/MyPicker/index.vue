2ad11516f80e2928d473bc433722216127d7abbe
White <1462449970@qq.com>
2021-05-04 21:31:39 +0800

    init

---
+++ src/components/MyPicker/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <picker-view v-if="visible" :indicator-style="indicatorStyle" :value="value" @change="bindChange">
+    <picker-view-column>
+      <view class="item" v-for="(item,index) in years" :key="index">{{item}}年</view>
+    </picker-view-column>
+  </picker-view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
